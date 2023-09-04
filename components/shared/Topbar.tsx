0c84a2bc0585375925706f650caa047a8973f06f
caioalvess <caio_f.alves@hotmail.com>
2023-09-03 21:13:51 -0300

    feat: topbar component

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export function Topbar() {
     return (
-        <h1>Topbar</h1>
+        <nav className="topbar">
+            <Link href="/" className="flex items-center gap-4">
+                <Image src="/logo.svg" alt="logo" width={28} height={28} />
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+            </Link>
+        </nav>
     )
 }
