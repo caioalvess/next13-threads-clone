25d7af16d4e36fe38712e8057f9c089ea4bca25c
caioalvess <caio_f.alves@hotmail.com>
2023-09-12 11:25:56 -0300

    perf: format some lines of topbar for better view

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function Topbar() {
-    return (
-        <nav className="topbar">
-            <Link href="/" className="flex items-center gap-4">
-                <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-                <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
-            </Link>
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image
+          src="/assets/logo.svg"
+          alt="logo"
+          width={28}
+          height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
 
-            <div className="flex items-center gap-1">
-            <div className='block md:hidden'>
+      <div className="flex items-center gap-1">
+        <div className='block md:hidden'>
           <SignedIn>
             <SignOutButton>
               <div className='flex cursor-pointer'>
@@ -26,12 +30,12 @@ export function Topbar() {
           </SignedIn>
         </div>
 
-                <OrganizationSwitcher appearance={{
-                    elements: {
-                        organizationSwitchrTrigger: "py-2 px-4"
-                    }
-                }} />
-            </div>
-        </nav>
-    )
+        <OrganizationSwitcher appearance={{
+          elements: {
+            organizationSwitchrTrigger: "py-2 px-4"
+          }
+        }} />
+      </div>
+    </nav>
+  )
 }
