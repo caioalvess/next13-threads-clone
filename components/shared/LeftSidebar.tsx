5a8b95c1f3b37e3011c54c489c0f736f6796a573
caioalvess <caio_f.alves@hotmail.com>
2023-09-12 11:24:03 -0300

    feat: leftsidebar component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,38 @@
+'use client'
+
+import Link from 'next/link'
+import { sidebarLinks } from '../../constants'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+
 export function LeftSidebar() {
+    const router = useRouter()
+    const pathname = usePathname()
+
     return (
-        <h1>LeftSidebar</h1>
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link, index) => {
+
+                    const isActive = (pathname.includes(link.route)
+                        && link.route.length > 1)
+                        || pathname === link.route
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={index}
+                            className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24} />
+                            <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+                        </Link>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
